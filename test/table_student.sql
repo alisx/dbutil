612f14482f871ba2afdf2b9e9b9a3149008d8e54
alisx <finddly@163.com>
2023-05-10 22:58:19 +0800

    调整MySql 测试方式

--- test/table_student.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `table_student` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `age` int(11) DEFAULT NULL,
-  `gender` varchar(255) DEFAULT NULL,
-  `class` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
